272f091240e86b1030e5907f23da441acada79f3
italic <italic.rendezvous@gmail.com>
2016-10-16 23:51:35 -0700

    PlayView: Copy/paste some template code from other tools.

--- py/ita_PlayView.py
+++ py/ita_PlayView.py
@@ -28,3 +28,44 @@ cmds.setFocus(panelname)
 scale elements by 0.15 (1920/1080 -> 288/162)
 
 """
+
+
+def help_call(*args):
+    """Open a text window with help information."""
+    helpText = (
+        'PlayView: Always play a specific viewport.\n'
+        'by Jeffrey "italic" Hoover'
+    )
+
+    if cmds.window(helpID, exists=True):
+        cmds.deleteUI(helpID)
+
+    cmds.window(helpID,
+                title='PlayViewHelp',
+                widthHeight=[300, 200],
+                sizeable=True,
+                resizeToFitChildren=True)
+    cmds.columnLayout(width=300)
+    cmds.scrollField(wordWrap=True,
+                     text=helpText,
+                     editable=False,
+                     width=300,
+                     height=200,
+                     font='smallPlainLabelFont')
+
+    cmds.showWindow()
+
+
+def destroy_window(*args):
+    """If PlayView windows exist, destroy them."""
+    if cmds.window(windowID, exists=True):
+        cmds.deleteUI(windowID)
+    if cmds.window(helpID, exists=True):
+        cmds.deleteUI(helpID)
+
+
+def prefs_play_all(*args):
+    if cmds.playbackOptions(q=True, v=True) == "all":
+        return False
+    else:
+        return True
