63ed998f80bd8088f998be298a3f37d3406cb085
italic <italic.rendezvous@gmail.com>
2017-06-06 23:59:29 -0700

    Butter: starting maya functions
    Initial exploration for getting/setting key data in maya.

--- py/ita_Butter/__init__.py
+++ py/ita_Butter/__init__.py
@@ -26,5 +26,28 @@ LogHandler.setFormatter(LogFormat)
 log.addHandler(LogHandler)
 log.setLevel(logging.DEBUG)
 
+
+def set_key_values(anim_curve=None, data=None):
+    for (ind, val) in enumerate(data):
+        anim_curve.setValue(ind, val)
+
+
+def get_key_values(anim_curve=None):
+    return [anim_curve.getValue(ind) for ind in range(anim_curve.numKeys() - 1)]
+
+
+def build_key_dict():
+    pass
+
+
+def get_curves():
+    if pmc.keyframe(q=True, sl=True, name=True):
+        crvs = [pmc.nodetypes.AnimCurve(crv) for crv in pmc.keyframe(q=True, sl=True, name=True)]
+    else:
+        crvs = [pmc.nodetypes.AnimCurve(crv) for crv in pmc.animCurveEditor("graphEditor1GraphEd", q=True, curvesShown=True)]
+
+    return crvs
+
+
 if __name__ == '__main__':
     pass
