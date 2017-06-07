f3a608874e80b741c66249c09fc8616f76435acd
italic <italic.rendezvous@gmail.com>
2017-06-06 23:55:01 -0700

    Butter: interface with scipy
    Data passed into interface functions are pure python. Data returned is pure Python. SciPy uses Numpy internally, so returns numpy.ndarray types back.

--- py/ita_Butter/ButterUI.py
+++ py/ita_Butter/ButterUI.py
@@ -11,7 +11,12 @@ class ButterWindow(QtWidgets.QMainWindow):
     """Main Window."""
 
     def __init__(self, parent=None):
-        pass
+        """:param parent: Window to place Butter under."""
+        super(ButterWindow, self).__init__(parent=parent)
+        self.settings = QtCore.QSettings("italic", "Butter")
+        self.__setup_ui()
+        self.move(self.settings.value("mainwindowposition", QtCore.QPoint(0, 0)))
+        self._ButterHelp = None
 
     def __setup_ui(self):
         self.setObjectName("ButterWindow")
@@ -26,13 +31,37 @@ class ButterWindow(QtWidgets.QMainWindow):
 
         self.LayoutVert1 = QtWidgets.QVBoxLayout(self.virticalLayoutWidget)
 
+    def closeEvent(self, *args, **kwargs):
+        """Custom closeEvent to write settings to files."""
+        self.settings.setValue("mainwindowposition", self.pos())
+        super(ButterWindow, self).closeEvent(*args, **kwargs)
+
+    def show_help_ui(self):
+        """Show help window."""
+        if self._ButterHelp is None:
+            self._ButterHelp = ButterHelpWindow()
+        self._ButterHelp.show()
+
 
 class ButterHelpWindow(QtWidgets.QMainWindow):
 
     """Help window."""
 
     def __init__(self, parent=None):
+        self.helpText = (
+            ""
+        )
+        self.settings = QtCore.QSettings("italic", "Butter")
+        self.__setup_ui()
+        self.move(self.settings.value("helpwindowposition", QtCore.QPoint(0, 0)))
+
+    def __setup_ui(self):
         pass
+
+    def closeEvent(self, *args, **kwargs):
+        """Custom closeEvent to write settings to file."""
+        self.settings.setValue("helpwindowposition", self.pos())
+        super(ButterHelpWindow, self).closeEvent(*args, **kwargs)
 
 
 if __name__ == '__main__':
@@ -40,3 +69,6 @@ if __name__ == '__main__':
         win = QtWidgets.QApplication([])
     except RuntimeError:
         win = QtCore.QCoreApplication.instance()
+
+    _ButterWin = ButterWindow()
+    _ButterWin.show()
--- py/ita_Butter/scipy_interface.py
+++ py/ita_Butter/scipy_interface.py
@@ -1,7 +1,59 @@
+"""
+Interface with SciPy filter functions.
+
+Data passed into this module is pure Python. Data is converted into usable form
+to pass into respective functions and returned as pure Python.
+"""
+
+import numpy
 import scipy.signal as sig
 from utils.qtshim import logging
 
 log = logging.getLogger(__name__)
 
-def create_filter():
-    pass
+
+def create_filter(fps, low, high, order, pass_type=None):
+    """
+    :param fps: Frequency of samples.
+    :param low: Low-end cutoff for highpass filter.
+    :param high: High-end cutoff for lowpass filter.
+    :param order: Order index of filter - higher order is sharper frequency response falloff.
+    :param pass_type: {"lowpass", "highpass", "bandpass", "bandstop"}
+
+    :return b, a: Polynomial arrays of the filter - to be plugged into sig.lfilter(b, a, x).
+    """
+    if pass_type == "lowpass":
+        critical = high
+    elif pass_type == "highpass":
+        critical = low
+    elif pass_type == "bandpass":
+        critical = [low, high]
+
+    nyq = 0.5 * float(fps)
+    low = float(low) / nyq
+    high = float(high) / nyq
+
+    b, a = sig.butter(order, critical, btype=pass_type, analog=True, output='ba')
+
+    b = b.tolist()
+    a = a.tolist()
+
+    return b, a
+
+
+def filter_list(b, a, data):
+    """
+    :param b: Numerator polynomial list of the filter.
+    :param a: Denominator polynomial list of the filter.
+    :param data: Python list of data to be filtered.
+
+    :return y: Python list of filtered data - converted from Numpy array.
+    """
+    b = numpy.asarray(b)
+    a = numpy.asarray(a)
+    data = numpy.asarray(data)
+
+    y = sig.filtfilt(b, a, data)
+    y = y.tolist()
+
+    return y
