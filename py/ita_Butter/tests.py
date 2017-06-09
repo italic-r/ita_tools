8e909e23db58aa5639f6fca665f086837393adaf
italic <italic.rendezvous@gmail.com>
2017-06-09 03:38:12 -0700

    Butter tests: use data from real maya scene to test against. Matplotlib shows filter is working properly, but maya's curves are not showing up in the same pattern.

--- py/ita_Butter/tests.py
+++ py/ita_Butter/tests.py
@@ -15,14 +15,58 @@ class TestScipy(unittest.TestCase):
 
     def test_array_create(self):
         """Test lfilter example."""
-        t = numpy.linspace(-1, 1, 201)
-        x = (
-            numpy.sin(2 * numpy.pi * 0.75 * t * (1 - t) + 2.1) +
-            0.1 * numpy.sin(2 * numpy.pi * 1.25 * t + 1) +
-            0.18 * numpy.cos(2 * numpy.pi * 3.85 * t)
-        )
-        xn_array = x + numpy.random.randn(len(t)) * 0.08
-        xn_list = xn_array.tolist()
+        t = numpy.linspace(-1, 1, 44)
+        x = [
+            6.10073773779645,
+            6.083901597959422,
+            6.772113729079406,
+            6.69155928387719,
+            5.843792396558136,
+            7.7600117770232195,
+            5.5412599302608205,
+            3.2762736016638847,
+            5.139264666004937,
+            4.904853857811264,
+            2.6942623221540942,
+            2.4209988926847212,
+            4.709816486018015,
+            3.8871745483887086,
+            1.9704382685500903,
+            0.5735523122411439,
+            1.9868888622241925,
+            -0.12216856465709647,
+            2.072497093710182,
+            -0.05054674399822545,
+            -0.4217189815610065,
+            -0.7959994546758562,
+            0.5847009212041216,
+            0.208866334410712,
+            1.3798892891393733,
+            1.0087164580062176,
+            -0.903095903962637,
+            0.38668886260055624,
+            -1.030400979158727,
+            -1.3735988710224327,
+            -2.757013888539267,
+            -3.0769011279372567,
+            -1.5865082764644143,
+            -1.8768666822355655,
+            -2.150130666858135,
+            -7.012036493552377,
+            -5.787785222699686,
+            -6.95854869976713,
+            -2.992713470883843,
+            -5.3361830190212896,
+            -5.4742396209005015,
+            -3.3983352920633285,
+            -2.6790412050488612,
+            -5.4469136434984256
+        ]
+        xn_array = numpy.asarray(x)
+        xn_list = x
+
+        print(xn_array)
+        print(xn_list)
 
         self.assertIsInstance(xn_array, numpy.ndarray)
         self.assertIsInstance(xn_list, list)
@@ -32,12 +76,15 @@ class TestScipy(unittest.TestCase):
     def test_lfilter_example(self):
         """Test lfilter example."""
         t, x, xn = self.test_array_create()
-        b, a = sig.butter(3, 0.05)
+        b, a = sig.butter(2, 0.05)
 
         zi = sig.lfilter_zi(b, a)
         z, _ = sig.lfilter(b, a, xn, zi=zi*xn[0])
         z2, _ = sig.lfilter(b, a, z, zi=zi*z[0])
         y = sig.filtfilt(b, a, xn)
+
+        print(t)
+        print(y)
 
         return t, x, xn, b, a, zi, z, z2, y
 
@@ -50,7 +97,6 @@ class TestScipy(unittest.TestCase):
         plt.plot(t, z, 'r--')
         plt.plot(t, z2, 'r')
         plt.plot(t, y, 'k')
-        # plt.plot(t, z, 'r--', t, z2, 'r', t, y, 'k')
         plt.legend(('noisy signal', 'lfilter, once', 'lfilter, twice', 'filtfilt'), loc='best')
         plt.grid(True)
         plt.show()
