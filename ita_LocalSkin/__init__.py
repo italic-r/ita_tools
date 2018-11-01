26269ae25d31565c60162efc145f78522b20cead
italic <italic.rendezvous@gmail.com>
2018-10-31 18:54:44 -0700

    LocalSkin: stub for future tool

---
+++ ita_LocalSkin/__init__.py
@@ -0,0 +1,6 @@
+"""
+Localize skin cluster deformation matrices.
+
+This tool allows you to quickly patch a rig's geometry to avoid loss of floating point precision
+at long distances from the scene origin. Load the geometry and joints and presto!
+"""
